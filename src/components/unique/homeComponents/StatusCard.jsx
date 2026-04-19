bce5c674b2fb9e8fab05bd868b6f24790485db30
protapchakma <protapchakma43@gmail.com>
2026-04-19 23:31:06 +0600

    hero section update

--- src/components/unique/homeComponents/StatusCard.jsx
+++ src/components/unique/homeComponents/StatusCard.jsx
@@ -1,6 +1,9 @@
 export default function StatusCard({ text, number }) {
     return (
-        <div className="bg-white p-8 flex flex-col items-center justify-center rounded-lg gap-2 text-center">
+
+        <div className="bg-white p-8 flex flex-col items-center 
+        justify-center rounded-lg gap-2 text-center">
+
             <h2 className="text-[#244D3F] font-semibold text-3xl">{number}</h2>
             <p className="text-[#64748B] text-lg">{text}</p>
         </div>
